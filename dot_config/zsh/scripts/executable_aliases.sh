b007cfcd788097d383d872a86429f6f1d872561b
virt-10 <121334908+virt-10@users.noreply.github.com>
2024-10-29 21:27:29 -0500

    revert(zsh): run flatpak zed

--- dot_config/zsh/scripts/executable_aliases.sh
+++ dot_config/zsh/scripts/executable_aliases.sh
@@ -2,7 +2,7 @@
 
 
 # aliases
-alias zed='zeditor -n '
+alias zed='flatpak run dev.zed.Zed -n '
 alias reload='systemctl --user daemon-reload'
 alias wall-set='~/.config/hypr/scripts/wall-set.sh'
 alias get-icons='wget -qO- https://git.io/papirus-icon-theme-install | env DESTDIR="$HOME/.local/share/icons" sh'
